5af20da707cbd5ca618a36f4e6d6dbb4812d7e6e
Alexei Colin <acolin@isi.edu>
2020-06-04 17:21:45 -0700

    bin: psalloc,psbatch: constraint as single arg

--- bin/pscommon.sh
+++ bin/pscommon.sh
@@ -14,35 +14,44 @@ constraint() {
 	local ARCH=$1
 	case "$ARCH" in
 	harpertown)
-		echo "E5410"
+		FEAT="E5410"
 		;;
 	westmere)
-		echo "X5650"
+		FEAT="X5650"
 		;;
 	sandybridge) # k20 ; ISI gpuk40c machine
-		echo "E5-2665"
+		FEAT="E5-2665"
 		;;
 	ivybridge)
-		echo "E5-2650v2"
+		FEAT="E5-2650v2"
 		;;
 	haswell) # k40
-		echo "E5-2640v3"
+		FEAT="E5-2640v3"
 		;;
 	broadwell) # p100
-		echo "E5-2640v4"
+		FEAT="E5-2640v4"
 		;;
 	skylake) # v100
-		echo "gold-6130|silver-4116"
+		FEAT="gold-6130|silver-4116"
 		;;
 	opteron) # Magny-Cours (no dedicated option); 'barcelona' ('amdfam10' is synonym) results in SIGILL
-		echo "AMD6176"
+		FEAT="AMD6176"
 		;;
 	any)
-		echo ""
+		FEAT=""
 		;;
 	*)
 		echo "ERROR: unknown architecture: ${ARCH}" 1>&2
 		exit 2
 		;;
 	esac
+	# Restrict to Infiniband nodes, because Myrinet nodes hang with /scratch
+	# See ITS Ticket RITM0188766. 
+	# Note that multiple --constraints args are not allowed (later override
+	# earlier) and --constraint cannot understand an expression (nested
+	# parens with # precedence), it gets converted into a list of features,
+	# with an AND/OR/XAND/XOR "modifier" attached to each feature. So, to
+	# achieve (f1|f2)&f3, passing 'f1|f2&f3' should work.
+	# See build_feature_list() in src/slurmctld/job_scheduler.c
+	echo "${FEAT}&IB"
 }
