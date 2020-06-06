228d391d33d66c36e910557eee019f6f0c1f676e
Alexei Colin <acolin@isi.edu>
2020-06-06 21:52:24 +0000

    jobs: test-prefix: test FEniCS too

--- exp/jobs/test-prefix.sh
+++ exp/jobs/test-prefix.sh
@@ -11,12 +11,15 @@ do
 	for nproc in 1 2
 	do
 		run mpirun -n $nproc python "${SELF_DIR}"/../apps/firedrake/matrix_free/stokes-casper.py 64 $solver 0 1
+		run mpirun -n $nproc python "${SELF_DIR}"/../apps/fenics/cavity/demo_cavity.py 64 $solver 0 1
 
 		if [[ "$solver" = "superlu_dist" ]]
 		then
 			eselect superlu_dist set superlu_dist_cuda
 		fi
+
 		run mpirun -n $nproc python "${SELF_DIR}"/../apps/firedrake/matrix_free/stokes-casper.py 64 $solver 1 1
+		run mpirun -n $nproc python "${SELF_DIR}"/../apps/fenics/cavity/demo_cavity.py 64 $solver 1 1
 
 		if [[ "$solver" = "superlu_dist" ]]
 		then
