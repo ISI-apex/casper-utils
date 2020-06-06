4967064c5206b90eda9599db5e5d0955e90e3894
Alexei Colin <acolin@isi.edu>
2020-06-05 21:56:59 -0400

    jobs: gpref: update snapshot to 20200604

--- jobs/gpref-sys.sh
+++ jobs/gpref-sys.sh
@@ -60,9 +60,9 @@ export DISTDIR="${DIST_PATH}"
 export USE_CPU_CORES=$JOBS
 export TODO=noninteractive
 export OFFLINE_MODE=$OFFLINE_MODE
-export SNAPSHOT_DATE=20200330
+export SNAPSHOT_DATE=20200604
 # TODO: have single date for both gentoo-*.tar.bz2 and portage-*.tar.gz2
-export SNAPSHOT_PORTAGE_DATE=20200211
+export SNAPSHOT_PORTAGE_DATE=20200604
 
 ## Workaround for failure to create a symlink at end of stage1
 mkdir -p $ROOT/tmp/var/db/repos/
