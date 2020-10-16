346aa9a664a96566b151db7e63bfe023041092c3
Alexei Colin <acolin@isi.edu>
2020-10-16 00:53:38 +0000

    exp/cahn-hilliard: makefile for theta dbgq runs

---
+++ exp/cahn-hilliard/theta/fch.sh
@@ -0,0 +1,13 @@
+# This is a separate script (not in Makefile) because it is spawned by mpirun.
+
+# Set the compiler vars (esp. CCVER) to prevent firedrake
+# from sniffing compiler version (which involves subprocess,
+# which forks, but fork breaks uGNI transport in OpenMPI).
+
+set -x
+
+BENCH_DIR=../../apps/firedrake-bench
+
+CXX=gcc CC=gcc CCVER=$(gcc -dumpversion | cut -d. -f-2) \
+	OMP_NUM_THREADS=1 \
+	exec python $(realpath ${BENCH_DIR}/cahn_hilliard/firedrake_cahn_hilliard_bare.py) "$@"
