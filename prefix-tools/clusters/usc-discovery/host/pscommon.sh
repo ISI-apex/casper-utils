59a1744b82afbaf0bdc83aec9090a3b560cbc847
Alexei Colin <acolin@isi.edu>
2020-10-26 03:57:14 +0000

    prefix-tools,ebuilds: fixes to pscommon.sh path

--- prefix-tools/clusters/usc-discovery/host/pscommon.sh
+++ prefix-tools/clusters/usc-discovery/host/pscommon.sh
@@ -1,2 +1,2 @@
 self_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "${self_dir}"/../prefix-tools/etc/prefixhelpers
+source "${EPREFIX}"/etc/prefix-tools/prefixhelpers
