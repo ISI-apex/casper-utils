6fd56ef2e97081801764fea63d5de3b05276939a
Alexei Colin <acolin@isi.edu>
2021-03-21 17:20:35 -0400

    exp/ch-scaling: constants for mesh size

--- exp/cahn-hilliard-scaling/plot-mesh-dist.py
+++ exp/cahn-hilliard-scaling/plot-mesh-dist.py
@@ -2,17 +2,31 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
+mesh = 8192
+theta_tpn = [24]
+summit_tpn = [40, 42]
+
+#mesh = 4096
+#theta_tpn = [48]
+#summit_tpn = [41]
+
 show_cols = ['cluster', 'mesh', 'ranks_per_node', 'total_s']
 
 d = pd.read_csv(sys.argv[1])
-d = d[(d['mesh'] == 8192) & (d['rank'] == 0)]
+d = d[(d['mesh'] == mesh) & (d['rank'] == 0)]
 d = d.sort_values('ranks')
 
 d['dsl_s'] = d['setup_s'] + d['solve_s']
 
-d_theta = d[(d['cluster'] == 'theta') & (d['ranks_per_node'] == 24)]
-print(d[d['cluster'] == 'summit'])
-d_summit = d[(d['cluster'] == 'summit') & ((d['ranks_per_node'] == 40) | (d['ranks_per_node'] == 42 ))]
+def calc_tpn_cond(tpns):
+    assert len(tpns) > 0
+    tpn_cond = d['ranks_per_node'] == tpns[0]
+    for i in range(1, len(tpns)):
+        tpn_cond = tpn_cond | (d['ranks_per_node'] == tpns[i])
+    return tpn_cond
+
+d_theta = d[(d['cluster'] == 'theta') & calc_tpn_cond(theta_tpn)]
+d_summit = d[(d['cluster'] == 'summit') & calc_tpn_cond(summit_tpn)]
 print(d_theta[show_cols])
 print(d_summit[show_cols])
 
@@ -38,7 +52,7 @@ ln_summit += ax_summit.plot(d_summit['ranks'], d_summit['total_s'], '--',
         marker='o', color='green')
 ax_summit.set_ylabel("Execution Time on Summit (s)")
 
-plt.title("Time to distribute 8K x 8K 2D mesh")
+plt.title(f"Time to distribute {mesh} x {mesh} 2D mesh")
 ax_theta.set_xlabel("Ranks")
 
 lns = ln_theta + ln_summit 
