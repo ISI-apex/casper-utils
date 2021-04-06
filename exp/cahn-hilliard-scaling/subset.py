3b09bcb78d4f0eb8d6e95f8ac7720cce6afe07bf
Alexei Colin <acolin@isi.edu>
2021-04-06 18:14:56 -0400

    exp/ch-scaling: subset.py: generalize with argparse

--- exp/cahn-hilliard-scaling/subset.py
+++ exp/cahn-hilliard-scaling/subset.py
@@ -1,6 +1,28 @@
 import sys
+import argparse
 import pandas as pd
 
-d = pd.read_csv(sys.argv[1])
-d = d[(d['mesh'] >= 4096) & (d['rank'] == 0)]
-d.to_csv(sys.argv[2])
+def csv_int_list(s):
+    return [int(n) for n in s.split(",")]
+
+parser = argparse.ArgumentParser(
+    description="Filter rows in a dataset")
+parser.add_argument('dataset',
+        help="File name of the input dataset (CSV)")
+parser.add_argument('--output', '-o', required=True,
+        help="Name of output file where to save filtered dataset")
+parser.add_argument('--mesh', type=csv_int_list,
+        help="Mesh sizes to include (comma-separated)")
+parser.add_argument('--rank', type=csv_int_list,
+        help="Ranks to include (comma-separated)")
+args = parser.parse_args()
+
+d = pd.read_csv(args.dataset)
+
+for mesh in args.mesh:
+    d = d[(d['mesh'] == mesh)]
+for rank in args.rank:
+    d = d[(d['rank'] == rank)]
+
+print(d)
+d.to_csv(args.output)
