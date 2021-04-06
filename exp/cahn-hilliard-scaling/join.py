c580c3eece1cfced54e1a43d94c3c061a21703a0
Alexei Colin <acolin@isi.edu>
2021-04-06 18:15:58 -0400

    exp/ch-scaling: join.py: generalize with argparse

--- exp/cahn-hilliard-scaling/join.py
+++ exp/cahn-hilliard-scaling/join.py
@@ -1,13 +1,23 @@
 import sys
+import argparse
 import pandas as pd
 
-d1 = pd.read_csv(sys.argv[1])
-d1['cluster'] = "theta"
-d2 = pd.read_csv(sys.argv[2])
-d2['cluster'] = "summit"
+parser = argparse.ArgumentParser(
+    description="Join datasets, creating an identifier column")
+parser.add_argument('datasets', nargs='*',
+        help="A pair of arguments for each dataset to be joined: name input_file")
+parser.add_argument('--output', '-o', required=True,
+        help="Name of output file where to save joined dataset")
+parser.add_argument('--column', '-c', required=True,
+        help="Name of column to create with identifier for each joined dataset")
+args = parser.parse_args()
 
-d = pd.concat([d1, d2])
+d = pd.DataFrame()
+for i in range(0, len(args.datasets), 2):
+    d1 = pd.read_csv(args.datasets[i + 1])
+    d1[args.column] = args.datasets[i]
+    d = pd.concat([d, d1])
+
 print(d)
-d.to_csv(sys.argv[3])
-
+d.to_csv(args.output)
 
