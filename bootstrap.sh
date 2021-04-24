c80cf5612334b217626e77aca74539cd5b5dd008
Alexei Colin <acolin@isi.edu>
2021-04-24 13:20:35 -0400

    bootstrap: init submodules, fetch LSF, unpack

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,63 +7,103 @@ run() {
 	"$@"
 }
 
-CLUSTER=$1
+function bootstrap_git_lfs() {
+	local mach="$(uname -m)"
+	case "$(uname -m)" in
+		ppc64le) arch=ppc64le;;
+		x86_64|amd64) arch=amd64;;
+		*)
+			echo "ERROR: unrecognized machine type from uname -m: ${mach}" 1>&2
+			exit 1
+			;;
+	esac
 
-if [[ -z "${CLUSTER}" ]]
-then
-	CLUSTER=generic
-fi
-
-case "${CLUSTER}" in
-	olcf-summit) ARCH=ppc64le;;
-	anl-theta|isi-gpuk40|generic) ARCH=amd64;;
-	*)
-		echo "ERROR: unkown platform" 1>&2
-		exit 1
-		;;
-esac
-
-if ! type -P tar >/dev/null
-then
-	echo "ERROR: tar required but not found" 1>&2
-	exit 2
-fi
-if ! type -P curl >/dev/null
-then
-	echo "ERROR: curl required but not found" 1>&2
-	exit 2
-fi
-
-GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download
-GIT_LFS_VER=2.13.3
-GIT_LFS_TAR=git-lfs-linux-${ARCH}-v${GIT_LFS_VER}.tar.gz
-GIT_LFS_MD5=28eafc12b75c29e0416b2cbb34e20758
-
-run mkdir -p git-lfs
-run cd git-lfs
-
-if [[ -f "${GIT_LFS_TAR}" ]]
-then
-	if type -P md5sum >/dev/null
+	if ! type -P tar >/dev/null
 	then
-		if [[ "$(md5sum ${GIT_LFS_TAR} | cut -d' ' -f1)" != "${GIT_LFS_MD5}" ]]
+		echo "ERROR: tar required but not found" 1>&2
+		exit 2
+	fi
+	if ! type -P curl >/dev/null
+	then
+		echo "ERROR: curl required but not found" 1>&2
+		exit 2
+	fi
+
+	local GIT_LFS_URL=https://github.com/git-lfs/git-lfs/releases/download
+	local GIT_LFS_VER=2.13.3
+	local GIT_LFS_TAR=git-lfs-linux-${arch}-v${GIT_LFS_VER}.tar.gz
+	local GIT_LFS_MD5
+	case "${arch}" in
+		amd64) GIT_LFS_MD5=28eafc12b75c29e0416b2cbb34e20758;;
+		ppc64le) GIT_LFS_MD5=a2da02c0931ff101a590383080f04089;;
+		*)
+			echo "ERROR: checksum not set for arch: ${arch}" 1>&2
+			exit 3
+			;;
+	esac
+
+	run mkdir -p git-lfs
+	run pushd git-lfs
+
+	if [[ -f "${GIT_LFS_TAR}" ]]
+	then
+		if type -P md5sum >/dev/null
 		then
-			rm "${GIT_LFS_TAR}"
+			if [[ "$(md5sum ${GIT_LFS_TAR} | cut -d' ' -f1)" != "${GIT_LFS_MD5}" ]]
+			then
+				rm "${GIT_LFS_TAR}"
+			fi
 		fi
 	fi
-fi
-if [[ ! -f "${GIT_LFS_TAR}" ]]
+	if [[ ! -f "${GIT_LFS_TAR}" ]]
+	then
+		run curl -LRO "${GIT_LFS_URL}/v${GIT_LFS_VER}/${GIT_LFS_TAR}"
+		if [[ "$(md5sum ${GIT_LFS_TAR} | cut -d' ' -f1)" != "${GIT_LFS_MD5}" ]]
+		then
+			echo "ERROR: md5sum mismatch on the downloaded ${GIT_LFS_TAR}" 1>&2
+			exit 4
+		fi
+	fi
+	run tar xf "${GIT_LFS_TAR}"
+
+	PREFIX_DIR=$PWD/prefix
+	mkdir -p "${PREFIX_DIR}"
+	PREFIX=${PREFIX_DIR} run ./install.sh
+
+	PATH=$PATH:${PREFIX_DIR}/bin
+
+	run git-lfs -v
+	popd
+	echo "Bootstrapped git-lfs successfully"
+}
+
+if type -P git-lfs >/dev/null && git-lfs -v >/dev/null
 then
-	run curl -LRO "${GIT_LFS_URL}/v${GIT_LFS_VER}/${GIT_LFS_TAR}"
+	echo "INFO: git-lfs already present: skipping bootstrap"
+
+	# Not a perfect check, but try to not run install unless necessary
+	if ! git config filter.lfs.required >/dev/null
+	then
+		echo "INFO: git-lfs present but not installed:" \
+			"installing temporarily"
+		run git lfs install
+		# Leave no trace: don't change the user's config
+		trap "run git lfs uninstall" EXIT
+	fi
+else
+	bootstrap_git_lfs # upstream script does 'git lfs install'
+	# Do not expose this bootstrapped git-lfs outside this script.
+	# If git-lfs was not installed, then git operations done after
+	# bootstrap will simply ignore LFS.
+	trap "run git lfs uninstall" EXIT
 fi
-run tar xf "${GIT_LFS_TAR}"
+# Do not expose this bootstrapped git-lfs outside this script;
+# if it's not installed
+trap "run git lfs uninstall" EXIT
 
-PREFIX_DIR=$PWD/prefix
-mkdir -p "${PREFIX_DIR}"
-PREFIX=${PREFIX_DIR} run ./install.sh
+run git submodule init
+run git submodule update
 
-PATH=$PATH:${PREFIX_DIR}/bin
-run git-lfs -v
+run git lfs pull
 
-echo "Bootstrapped git-lfs. Set your PATH to"
-echo "    PATH=\$PATH:${PREFIX_DIR}/bin"
+run ./unpack.sh
