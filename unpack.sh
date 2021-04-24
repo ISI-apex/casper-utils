75d0e1cd159a3686d73e92a9dfcff6da8e9aca8e
Alexei Colin <acolin@isi.edu>
2021-04-23 23:15:39 -0400

    pack/unpack: helpers for storing VCS clones in git lfs

---
+++ unpack.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# An helper to store VCS repos in distfiles/ in git-lfs (Large File Storage).
+# We could but don't want to add thousands of tiny files as LFS objects, so
+# these pack.sh/unpack.sh scripts pack those repos into tarballs.
+
+function is_dir_empty() {
+	typeset dir="${1:?required directory argument is missing}"
+	if [[ ! -d "${dir}" ]]
+	then
+		return 0
+	fi
+	set -- ${dir}/*
+	if [ "${1}" == "${dir}/*" ]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+run() {
+	echo "$@"
+	"$@"
+}
+
+DIST_DIR=distfiles
+VCS_DIR=git3-src
+
+if is_dir_empty "${DIST_DIR}"
+then
+	echo "ERROR: distdir/ is empty: did you fetch git LFS objects?" 1>&2
+	exit 1
+fi
+
+if is_dir_empty "${DIST_DIR}/${VCS_DIR}"
+then
+	# nothing to pack
+	exit 0
+fi
+
+if ! type -P tar >/dev/null
+then
+	echo "ERROR: tar is required but not found" 1>&2
+	exit 2
+fi
+
+for repo_tarball in "${DIST_DIR}/${VCS_DIR}"/*.tar
+do
+	repo_dir="${repo_tarball%.tar}"
+	if [[ -d "${repo_dir}" ]]
+	then
+		echo "ERROR: directory already exists: ${repo_dir}" 1>&2
+		exit 3
+	fi
+	run tar -C "${DIST_DIR}/${VCS_DIR}" -x -f "${repo_tarball}"
+done
